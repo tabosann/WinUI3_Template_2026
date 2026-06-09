fee07831a7560fac8adb2f2d17b8ee581da06caf
tabosann <tabosann@example.com>
2026-06-09 19:55:19 +0900

    [BUG]エンコーディングがANSI化してしまったり、リネームが不十分だったりした

--- Publish.ps1
+++ Publish.ps1
@@ -1,4 +1,4 @@
-cd WinUI3_Template_2026\
+﻿cd WinUI3_Template_2026\
 dotnet publish -p:PublishProfile=NoNativeAOT -p:Configuration=Release_Unpackaged -p:Platform=x64
 dotnet publish -p:PublishProfile=NativeAOT -p:Configuration=Release_Unpackaged -p:Platform=x64
 pause
--- Rename.ps1
+++ Rename.ps1
@@ -1,17 +1,17 @@
-param(
+﻿param(
 	[Parameter(Mandatory=$true)]
 	[string]$ProjectName
 )
 
 Get-ChildItem -File | select -ExpandProperty FullName | %{
-(Get-Content $_) -replace 'winui3_template_2026',"$ProjectName" | Set-Content $_
+(Get-Content $_) -replace 'winui3_template_2026',"$ProjectName" | Set-Content $_ -Encoding UTF8
 }
 
 Get-ChildItem -Directory -Exclude .git,.jj | Get-ChildItem -File -Recurse | select -ExpandProperty FullName | %{
-(Get-Content $_) -replace 'winui3_template_2026',"$ProjectName" | Set-Content $_
+(Get-Content $_) -replace 'winui3_template_2026',"$ProjectName" | Set-Content $_ -Encoding UTF8
 }
 
 Get-ChildItem -Recurse -Filter '*winui3_template_2026*' | %{
-$name = ($_ | select -ExpandProperty Name) -replace 'winui3_template_2026','TestProject'
-Rename-Item $_ -NewName $name
+$name = $_.Name -replace 'winui3_template_2026',"$ProjectName"
+Rename-Item $_.FullName -NewName $name
 }
